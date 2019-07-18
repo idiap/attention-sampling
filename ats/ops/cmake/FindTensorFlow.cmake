9e0f3d77dccf497138d928f16ccd7fc813be3de8
Angelos Katharopoulos <angelos.katharopoulos@idiap.ch>
2019-07-18 15:42:57 +0200

    Warn about TF compiler incompatibility

--- ats/ops/cmake/FindTensorFlow.cmake
+++ ats/ops/cmake/FindTensorFlow.cmake
@@ -21,9 +21,9 @@ execute_process(
 )
 execute_process(
     COMMAND python -c "import tensorflow as tf; \
-    lib = next(f for f in tf.sysconfig.get_link_flags() \
-               if f.startswith('-l')); \
-    print(lib[2:] if lib[2] != ':' else lib[3:], end='')"
+        lib = next(f for f in tf.sysconfig.get_link_flags() \
+                   if f.startswith('-l')); \
+        print(lib[2:] if lib[2] != ':' else lib[3:], end='')"
     OUTPUT_VARIABLE TENSORFLOW_LIB_NAME
 )
 find_library(
@@ -31,6 +31,41 @@ find_library(
     PATHS ${TENSORFLOW_LIB_DIR}
 )
 message(STATUS "Found TensorFlow lib: " ${TENSORFLOW_LIB})
+
+# Check for issue #27067 in TensorFlow. Basically make sure that if we have
+# version > 1.13 we are using gcc < 5.
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    if (NOT (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5))
+        execute_process(
+            COMMAND python -c "import tensorflow as tf; \
+                print(tf.__version__, end='')"
+            OUTPUT_VARIABLE TF_VERSION
+        )
+        if(NOT (TF_VERSION VERSION_LESS 1.14))
+            if(DEFINED ENV{TF_COMPILER_VERSION_OVERRIDE})
+                message(WARNING "You are using an unsupported compiler! \
+                                 You have manually overriden our check and \
+                                 your extension might break in undefined \
+                                 ways. To remove the override unset the \
+                                 TF_COMPILER_VERSION_OVERRIDE environment \
+                                 variable")
+            else()
+                message(FATAL_ERROR "You are using an unsupported compiler! \
+                                     TensorFlow extensions for versions >= 1.14 \
+                                     must be build with gcc < 5. To override set \
+                                     TF_COMPILER_VERSION_OVERRIDE environment \
+                                     variable.")
+            endif()
+        else()
+            message(WARNING "You are using an unsupported compiler! It is \
+                             recommended to build your TensorFlow extensions \
+                             with gcc 4.8 .")
+        endif()
+    endif()
+else()
+    message(WARNING "Unless you are using a custom built TensorFlow it \
+                     is suggested to use gcc to compile your extensions")
+endif()
 
 # Add the CXX flags
 execute_process(
